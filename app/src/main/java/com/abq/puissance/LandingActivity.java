03022af078cb29f91fb6539930095040ca331f1c
Abdel-Qahar Traore <abdelqahar.traore@gmail.com>
2021-05-26 20:50:57 +0000

    implemented shared preferences

--- app/src/main/java/com/abq/puissance/LandingActivity.java
+++ app/src/main/java/com/abq/puissance/LandingActivity.java
@@ -1,6 +1,8 @@
 package com.abq.puissance;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.abq.puissance.models.Matiere;
@@ -9,11 +11,17 @@ import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.abq.puissance.databinding.ActivityLandingBinding;
 
@@ -22,6 +30,14 @@ public class LandingActivity extends AppCompatActivity {
     private ActivityLandingBinding binding;
     private Button btn_puissance;
     private Button btn_matiere;
+    private Button btn_menu;
+    private Button btn_comming;
+    private final static int MENU_AJOUTER = Menu.FIRST;
+    private final static int MENU_SUPPRIMER = Menu.FIRST + 1;
+
+    private Button btnSharedPrefs;
+    private EditText editSharedPrefs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,9 +51,16 @@ public class LandingActivity extends AppCompatActivity {
 
         btn_puissance = findViewById(R.id.btn_puissance);
         btn_matiere = findViewById(R.id.btn_matiere);
+        btn_comming = findViewById(R.id.btn_comming);
+        btn_menu = findViewById(R.id.btn_menu);
+        editSharedPrefs = findViewById(R.id.edit_shared_prefs);
+        btnSharedPrefs = findViewById(R.id.btn_shared_prefs);
 
-//        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-//        toolBarLayout.setTitle(getTitle());
+        registerForContextMenu(btn_menu);
+
+        setTitle("DevMobileApp");
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
 
 //        FloatingActionButton fab = binding.fab;
 
@@ -50,11 +73,16 @@ public class LandingActivity extends AppCompatActivity {
             toMatiereForm();
         });
 
+        btn_comming.setOnClickListener(v -> {
+            Snackbar.make(v, "A venir...", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+        });
 
-//        btn_coming.setOnClickListener(v -> {
-//            Snackbar.make(v, "A venir...", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show();
-//        });
+        applySharedPreferences();
+
+        btnSharedPrefs.setOnClickListener(v -> {
+            saveMesPrefs();
+        });
     }
 
     private void toPuissance() {
@@ -72,5 +100,44 @@ public class LandingActivity extends AppCompatActivity {
     private void toMatiereForm() {
         Intent matiere_page = new Intent(this, MatiereFormActivity.class);
         startActivity(matiere_page);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.add(menu.NONE, MENU_AJOUTER, Menu.NONE, "Ajouter un élément");
+        menu.add(menu.NONE, MENU_SUPPRIMER, Menu.NONE, "Supprimer un élément");
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case MENU_AJOUTER:
+                Toast.makeText(this, "Ajout...", Toast.LENGTH_SHORT).show();
+            case MENU_SUPPRIMER:
+                Toast.makeText(this, "Suppression...", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onContextItemSelected(item);
+    }
+
+    private void applySharedPreferences() {
+//        SharedPreferences mesPrefs = getSharedPreferences("sharedPreferencesFile", Context.MODE_PRIVATE);
+        SharedPreferences mesPrefs = getPreferences(Context.MODE_PRIVATE);
+        String leCompteur = mesPrefs.getString("compteur", "0_0");
+        if (!leCompteur.equals("0_0")) {
+            editSharedPrefs.setText(String.valueOf(leCompteur));
+        }
+    }
+
+    private void saveMesPrefs() {
+        SharedPreferences mesPrefs = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mesPrefs.edit();
+        editor.putString("compteur", editSharedPrefs.getText().toString());
+        boolean verif = editor.commit();
+
+        if (verif)
+            Toast.makeText(this, "Enregistré", Toast.LENGTH_SHORT).show();
+        else Toast.makeText(this, "Erreur lors de l'enregistrement", Toast.LENGTH_SHORT).show();
     }
 }
